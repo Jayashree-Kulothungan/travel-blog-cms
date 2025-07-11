68baa5860437847566782c2051466872fda37884
Jayashree-Kulothungan <jayashreekulothungan@gmail.com>
2025-07-11 05:18:09 -0300

    fix: appoloclient env variable

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 const client = new ApolloClient({
   ssrMode: typeof window === 'undefined',
   link: new HttpLink({
-    uri: 'http://travel-blog-cms.local/graphql',
+    uri: process.env.WPGRAPHQL_URL ?? '',
     fetch,
   }),
   cache: new InMemoryCache(),
